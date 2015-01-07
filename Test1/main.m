bdaec66415bdcde7946af524fccc7b19f5329873
Xiaodong Qian <StevenQian@campus-054-203.ucdavis.edu>
2015-01-07 10:08:41 -0800

    Initial Commit

---
+++ Test1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test1
+//
+//  Created by Xiaodong Qian on 1/7/15.
+//  Copyright (c) 2015 Xiaodong Qian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
